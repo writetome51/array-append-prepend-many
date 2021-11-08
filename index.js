464d7d2d8362a086d33d4f2b8b1f5066f96cbb57
Steve Thompson <writetome51@gmail.com>
2021-11-07 17:08:41 -0700

    Made functions faster

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 export const appendMany = (values, array) => {
-	array.push(...values);
+	// Tested to be faster than array.push(...values):
+	for (
+		let i = 0, lastI = array.length - 1, valsLen = values.length;
+		i < valsLen;
+		++i
+	) {
+		array[++lastI] = values[i];
+	}
 }
 
 
 export const prependMany = (values, array) => {
-	array.unshift(...values);
+	// Tested to be faster than array.unshift(...values):
+	array.splice(0, 0, ...values)
 }
--- tests.js
+++ tests.js
@@ -27,7 +27,53 @@ console.log(arr);
 // [ -2, -1, 0, 1, 2, 3, 4]
 
 
-
 prependMany([[-2], [-1]], arr);
 console.log(arr);
 // [ [-2], [-1] -2, -1, 0, 1, 2, 3, 4]
+
+
+
+
+
+
+
+
+//speed comparison between .push() and loop using simple assignment:
+
+//Faster:
+arr = [];
+let ms = Date.now();
+for (let i=0; i<50000000; ++i){
+	arr.push(0);
+}
+console.log(Date.now() - ms);
+
+
+arr = [];
+ms = Date.now();
+for (let i=0, len=arr.length; i<50000000; ++i){
+	arr[len] = 0;
+	++len;
+}
+console.log(Date.now() - ms);
+
+
+
+//speed comparison between .unshift() and .splice():
+
+arr = [];
+ms = Date.now();
+for (let i=0; i<190000; ++i){
+	arr.unshift(0,0,0);
+}
+console.log(Date.now() - ms);
+let firstLen = arr.length;
+
+
+arr = [];
+ms = Date.now();
+for (let i=0; i<190000; ++i){
+	arr.splice(0,0, 0,0,0);
+}
+console.log(Date.now() - ms);
+console.log(arr.length === firstLen);
